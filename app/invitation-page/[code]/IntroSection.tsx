c2d8a052c1b006c87fda5dc73b1ae1b815c62acf
Christopher Sonny <christomurasu@gmail.com>
2026-07-08 17:28:18 +0700

    fix: intro section

--- app/invitation-page/[code]/IntroSection.tsx
+++ app/invitation-page/[code]/IntroSection.tsx
@@ -12,7 +12,6 @@ export default function IntroSection({
   const textAlign = align === "left" ? "left" : "right"
   const textSide = align === "left" ? "flex-start" : "flex-end"
   const photoSide = align === "left" ? "flex-end" : "flex-start"
-  const stampSize = 165
   const tr = t[lang]
 
   return (
@@ -26,31 +25,31 @@ export default function IntroSection({
         @font-face { font-family: 'Sloop'; src: url('/fonts/Sloop-ScriptThree.ttf') format('truetype'); }
       `}</style>
 
-      <div style={{ flex: 1, display: "flex", flexDirection: "column", alignItems: textSide, justifyContent: "center", padding: "32px 28px", textAlign }}>
+      <div style={{ flex: 1, display: "flex", flexDirection: "column", alignItems: textSide, justifyContent: "center", padding: "3dvh 28px 1dvh", textAlign }}>
         {showGreeting && (
           <>
-            <p style={{ marginBottom: 6 }}>
-              <span style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500, fontSize: 15, color: "#5F5F5F" }}>{tr.dearGuest} </span>
-              <span style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 700, fontSize: 15, color: "#5F5F5F" }}>{guestGreeting},</span>
+            <p style={{ marginBottom: "0.8dvh" }}>
+              <span style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500, fontSize: "clamp(11px, 1.8dvh, 15px)", color: "#5F5F5F" }}>{tr.dearGuest} </span>
+              <span style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 700, fontSize: "clamp(11px, 1.8dvh, 15px)", color: "#5F5F5F" }}>{guestGreeting},</span>
             </p>
-            <p style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500, fontSize: 15, color: "#5F5F5F", marginBottom: 20, maxWidth: 260 }}>
+            <p style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500, fontSize: "clamp(11px, 1.8dvh, 15px)", color: "#5F5F5F", marginBottom: "2dvh", maxWidth: 260 }}>
               {tr.invitedTo}
             </p>
           </>
         )}
         <h1 style={{
           fontFamily: "'Sloop', cursive",
-          fontSize: 64, fontWeight: 400,
+          fontSize: "clamp(42px, 8dvh, 64px)", fontWeight: 400,
           color: "#5F5F5F", lineHeight: 1,
-          marginBottom: align === "left" ? 16 : 30,
-          marginTop: align === "left" ? 0 :90
+          marginBottom: align === "left" ? "1.5dvh" : "2.5dvh",
+          marginTop: align === "left" ? 0 : "6dvh"
         }}>
           {name}
         </h1>
-        <p style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500, fontSize: 15, color: "#5F5F5F", marginBottom: 4 }}>
+        <p style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 500, fontSize: "clamp(11px, 1.8dvh, 15px)", color: "#5F5F5F", marginBottom: "0.5dvh" }}>
           {parentLabel}
         </p>
-        <p style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 700, fontSize: 15, color: "#5F5F5F", maxWidth: 260 }}>
+        <p style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 700, fontSize: "clamp(11px, 1.8dvh, 15px)", color: "#5F5F5F", maxWidth: 260 }}>
           {parentNames}
         </p>
       </div>
@@ -61,24 +60,34 @@ export default function IntroSection({
             width: "55%", aspectRatio: "231 / 280",
             marginRight: align === "left" ? 28 : 0,
             marginLeft: align === "right" ? 28 : 0,
-            marginBottom: 28, marginTop: 28, position: "relative"
+            marginBottom: "3dvh", marginTop: "2dvh", position: "relative"
           }}>
             {stampUrl && align === "left" && (
-              <img src={stampUrl} alt="Stamp" style={{ position: "absolute", top: 0, left: 0, width: stampSize, height: stampSize, objectFit: "contain", transform: "translate(-50%, -50%) rotate(-10deg)", zIndex: 2 }} />
+              <img src={stampUrl} alt="Stamp" style={{
+                position: "absolute", top: 0, left: 0,
+                width: "clamp(100px, 20dvh, 165px)" as unknown as number,
+                height: "clamp(100px, 20dvh, 165px)" as unknown as number,
+                objectFit: "contain",
+                transform: "translate(-50%, -50%) rotate(-10deg)", zIndex: 2
+              }} />
             )}
             {stampUrl && align === "right" && (
-                <img src={stampUrl} alt="Stamp"
-                style={{
-                  position: "absolute",
-                  bottom: 30, right: 0,
-                  width: stampSize, height: stampSize,
-                  objectFit: "contain",
-                  transform: "translate(50%, 50%) rotate(10deg)",
-                  zIndex: 2
-                }} />
-
+              <img src={stampUrl} alt="Stamp" style={{
+                position: "absolute",
+                bottom: 30, right: 0,
+                width: "clamp(100px, 20dvh, 165px)" as unknown as number,
+                height: "clamp(100px, 20dvh, 165px)" as unknown as number,
+                objectFit: "contain",
+                transform: "translate(50%, 50%) rotate(10deg)", zIndex: 2
+              }} />
             )}
-            <p style={{ position: "absolute", top: -24, right: align === "left" ? 0 : "auto", left: align === "right" ? 0 : "auto", fontFamily: "'Poppins', sans-serif", fontWeight: 700, fontStyle: "italic", fontSize: 14, color: "#5F5F5F", whiteSpace: "nowrap", zIndex: 2 }}>
+            <p style={{
+              position: "absolute", top: -24,
+              right: align === "left" ? 0 : "auto",
+              left: align === "right" ? 0 : "auto",
+              fontFamily: "'Poppins', sans-serif", fontWeight: 700, fontStyle: "italic",
+              fontSize: "clamp(11px, 1.6dvh, 14px)", color: "#5F5F5F", whiteSpace: "nowrap", zIndex: 2
+            }}>
               {roleLabel}
             </p>
             <div style={{ width: "100%", height: "100%", overflow: "hidden", position: "relative" }}>
