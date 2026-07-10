a8e55b424520451422aefc609246a08a8272d045
Christopher Sonny <christomurasu@gmail.com>
2026-07-10 11:36:51 +0700

    fixed logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import "./globals.css"
 
 export const metadata: Metadata = {
   title: "SF Invitation",
-  description: "Digital Wedding Invitation",
+  description: "Digital Wedding Invitations",
+  icons: {
+    icon: "https://ervlyplqjeszzmetmcsl.supabase.co/storage/v1/object/public/wedding-photos/96fdd251-193e-46f5-9c3d-0b1db3b20f5a/logo_url-1782405949522.png",
+  }
 }
 
 export default function RootLayout({
